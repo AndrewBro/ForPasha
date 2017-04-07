bcb20a6844fb0139e10ffa16e05f5df1603c0ec7
Your Name Comes Here <beliverwww@gmail.com>
2017-04-07 16:02:24 +0300

    for Pasha

--- app.js
+++ app.js
@@ -1,21 +1,97 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ui.router']);
 
-myApp.config(function($routeProvider){
-    $routeProvider
-        .when('/', {
-            templateUrl: 'pages/main.html',
-            controller: 'mainController'
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: './pages/main.html',
+            controller: 'mainCtrl'
         })
-        .when('/second', {
-            templateUrl: 'pages/second.html',
+        .state('first',{
+            url: '/first',
+            templateUrl: './pages/first.html',
+            controller: 'firstCtrl',
+            views: {
+                'more': {
+                    templateUrl: 'more.html',
+                    controller: function($scope){ button }
+                },
+                'contacts': {
+                    templateUrl: 'contacts.html',
+                    controller: function($scope){ button }
+                },
+                'address': {
+                    templateUrl: 'address.html',
+                    controller: function($scope){ button }
+                }
+            }
+        })
+        .state('second', {
+            url: '/second',
+            templateUrl: './pages/second.html',
             controller: 'secondCtrl'
         })
+        .state('third', {
+            url: '/third',
+            templateUrl: './pages/third.html',
+            controller: 'thirdCtrl'
+        })
+        .state('fourth', {
+            url: '/fourth',
+            templateUrl: './pages/fourth.html',
+            controller: 'fourthCtrl'
+        });
+
+    $urlRouterProvider.otherwise('/');
+
 });
 
-myApp.controller('mainCtrl', function($scope){
-
+myApp.controller('mainCtrl', function ($scope) {
+});
+myApp.controller('firstCtrl', function ($scope) {
+});
+myApp.controller('secondCtrl', function ($scope) {
+});
+myApp.controller('thirdCtrl', function ($scope) {
+});
+myApp.controller('fourthCtrl', function ($scope) {
 });
 
-myApp.controller('secondCtrl', function($scope){
+//
+// var button = angular.module('button', ['ui.router']);
+//
+// button.config(function ($stateProvider, $urlRouterProvider) {
+//
+//     $stateProvider
+//         .state('more', {
+//             url: '/more',
+//             view: {templateUrl: './pages/more.html'},
+//             // templateUrl: './pages/more.html',
+//             controller: 'moreCtrl'
+//         })
+//         .state('contacts', {
+//             url: '/contacts',
+//             view: {templateUrl: './pages/contacts.html'},
+//             // templateUrl: './pages/contacts.html',
+//             controller: 'contactsCtrl'
+//         })
+//         .state('address', {
+//             url: '/address',
+//             view: {templateUrl: './pages/address.html'},
+//             // templateUrl: './pages/address.html',
+//             controller: 'addressCtrl'
+//         });
+//
+//     $urlRouterProvider.otherwise('/');
+//
+// });
 
-});
+// button.controller('moreCtrl', function ($scope) {
+// });
+// button.controller('contactsCtrl', function ($scope) {
+// });
+// button.controller('addressCtrl', function ($scope) {
+// });
+
+
--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res){
     res.sendfile('index.html');
 });
 
-http.listen(3000, function(){
+http.listen(5000, function(){
     console.log('listening on *:3000');
 });
